f3a7cfa96fd3b3c0123c81062de5acb169be6505
Joseph <limjoseph2004@gmail.com>
2023-12-13 22:43:27 +1100

    Implement DOM and JS board interactions and representations

--- script.js
+++ script.js
@@ -1,40 +1,90 @@
 game = (function () {
-	function Box() {
-		// Init box
-		let marker;
-		const box = document.createElement("div");
-		box.classList.add("box");
+	function Tile(initIndex) {
+		// Init tile
+		let marker = 0;
+		const index = initIndex;
+		const tile = document.createElement("div");
+		tile.classList.add("tile");
+		tile.setAttribute("index", index);
 
 		// Functions
 		function setMarker(newMarker) {
-			marker = newMarker;
+			if (marker === 0) {
+				marker = newMarker;
+			}
 		}
 
 		function getMarker() {
 			return marker;
 		}
 
-		function getBox() {
-			return box;
+		function getTile() {
+			return tile;
 		}
 
-		return { setMarker, getMarker, getBox };
+		function getIndex() {
+			return index;
+		}
+
+		return { setMarker, getMarker, getTile, getIndex };
 	}
+
+	const player1 = (function () {
+		const marker = "red";
+
+		function getMarker() {
+			return marker;
+		}
+
+		return { getMarker };
+	})();
+
+	const player2 = (function () {
+		const marker = "blue";
+
+		function getMarker() {
+			return marker;
+		}
+
+		return { getMarker };
+	})();
 
 	// Cache DOM
-	const container = document.querySelector(".wrapper");
-	console.log(container);
+	const boardNode = document.querySelector(".board");
+	const tileNodes = boardNode.childNodes;
 
 	// Init game
-	fillContainer();
+	let board = [];
+	let currentPlayer = player1;
+	fillBoard();
+
+	// Bind events
+	boardNode.addEventListener("click", (e) => {
+		setTileMarker(e);
+	});
 
 	// Functions
-	function fillContainer() {
-		for (let i = 0; i < 9; i++) {
-			box = Box();
-			container.append(box.getBox());
+	function fillBoard() {
+		for (let index = 0; index < 9; index++) {
+			const tile = Tile(index);
+			board.push(tile);
+			boardNode.append(tile.getTile());
 		}
 	}
 
-	return;
+	function setTileMarker(e) {
+		const tileIndex = e.target.getAttribute("index");
+		const tile = board[tileIndex];
+
+		tile.setMarker(currentPlayer.getMarker());
+		console.log(tile.getMarker());
+		render();
+	}
+
+	function render() {
+		console.log(tileNodes);
+		tileNodes.forEach((tileNode, index) => {
+			tileNode.style.backgroundColor = board[index].getMarker();
+		});
+	}
 })();
